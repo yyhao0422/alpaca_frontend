c5f333353812865b35bac918a5d43aee3eb7a0fc
Yong Yung Hao <yyhao0422@gmail.com>
2024-03-18 09:03:19 +0800

    add basename

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { router } from "./router";
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <RouterProvider router={router} basename="/" />
     </>
   );
 }
